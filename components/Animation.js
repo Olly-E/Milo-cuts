ac522d45c780da874f0bed3d5c6c53e184624ba9
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-11-10 11:04:17 -0800

    modal showCase added

--- components/Animation.js
+++ components/Animation.js
@@ -17,10 +17,11 @@ export const modalAnimation = {
 
 export const container = {
     hidden: { opacity: 1, scale: 0 },
-    visible: {
+    show: {
       opacity: 1,
       scale: 1,
       transition: {
+        // delay: 1,
         delayChildren: 0.3,
         staggerChildren: 0.2
       }
@@ -29,8 +30,23 @@ export const container = {
 
   export const itemImg = {
     hidden: { y: 20, opacity: 0 },
-    visible: {
+    show: {
       y: 0,
       opacity: 1
     }
   };
+
+  export const imagesAnim = {
+    hidden: {
+      display:'none',
+      opacity: 0
+    },
+    show: {
+      display: 'grid',
+      opacity: 1,
+      transition: {
+        delay: 1
+      }
+    }
+  }
+
--- components/GridModal.js
+++ components/GridModal.js
@@ -1,36 +1,123 @@
 import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { itemImg, modalAnimation } from './Animation';
+import { container, imagesAnim, itemImg, modalAnimation } from './Animation';
 import styled from "styled-components";
 import { ImageList, ImageListItem } from '@mui/material';
-import style1 from '../assets/style1.jpg';
 import style2 from '../assets/style2.jpg';
 import style5 from '../assets/style5.jpg';
 import style6 from '../assets/style6.jpg';
+import show1 from '../assets/show1.jpg';
+import show2 from '../assets/show2.jpg';
+import show3 from '../assets/show3.jpg';
+import show4 from '../assets/show4.jpg';
+import show5 from '../assets/show5.jpg';
+import show6 from '../assets/show6.jpg';
+import show7 from '../assets/show7.jpg';
+import show8 from '../assets/show8.jpg';
+import show9 from '../assets/show9.jpg';
+import show10 from '../assets/show10.jpg';
+import show11 from '../assets/show11.jpg';
+import show12 from '../assets/show12.jpg';
+import show13 from '../assets/show13.jpg';
+import show14 from '../assets/show14.jpg';
+import show15 from '../assets/show15.jpg';
+import show16 from '../assets/show16.jpg';
+import show17 from '../assets/show17.jpg';
+import show19 from '../assets/show19.jpg';
+import show20 from '../assets/show20.jpg';
+import show21 from '../assets/show21.jpg';
 import Image from 'next/image';
 import {MdKeyboardBackspace} from 'react-icons/md'
 
 
 export default function GridModal({setShowModal}) {
+  
 
   const itemData = [
     {
-    imgTitle: 'img1',
-    img: style1
+    imgTitle: 'show1',
+    img: show1
+  },
+    {
+    imgTitle: 'show2',
+    img: show2
+  },
+    {
+    imgTitle: 'show3',
+    img: show3
+  },
+    {
+    imgTitle: 'show4',
+    img: show4
+  },
+    {
+    imgTitle: 'show12',
+    img: show12
+  },
+    {
+    imgTitle: 'show13',
+    img: show13
+  },
+    
+    {
+    imgTitle: 'show14',
+    img: show14
+  },
+    {
+    imgTitle: 'show15',
+    img: show15
+  },
+    {
+    imgTitle: 'show5',
+    img: show5
+  },
+    {
+    imgTitle: 'show6',
+    img: show6
+  },
+    {
+    imgTitle: 'show7',
+    img: show7
   },
   {
-    imgTitle: 'img2',
-    img: style2
+    imgTitle: 'show8',
+    img: show8
   },
   {
-    imgTitle: 'img3',
-    img: style5
+    imgTitle: 'show9',
+    img: show9
   },
   {
-    imgTitle: 'img4',
-    img: style6
+    imgTitle: 'show10',
+    img: show10
+  },
+  {
+    imgTitle: 'show11',
+    img: show11
+  },
+  {
+    imgTitle: 'show16',
+    img: show16
+  },
+  {
+    imgTitle: 'show17',
+    img: show17
+  },
+  {
+    imgTitle: 'show19',
+    img: show19
+  },
+  {
+    imgTitle: 'show20',
+    img: show20
+  },
+  {
+    imgTitle: 'show21',
+    img: show21
   },
 ]
+
+
 
   return (
     <AnimatePresence>
@@ -40,25 +127,41 @@ export default function GridModal({setShowModal}) {
       animate="visible"
       exit="exit"
     >
-      <div>
-        <div className='back' onClick={() => setShowModal(false)}><MdKeyboardBackspace/></div>
+      <motion.div
+        
+      >
+        <motion.div className='back' onClick={() => setShowModal(false)} 
+          initial={{opacity: 0, x: '15px'}}
+          animate={{opacity: 1, x: '0', transition:{delay: 0.5, duration: 0.5}}}
+        >
+        <MdKeyboardBackspace/></motion.div>
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
           <ImageList variant="masonry" cols={3} gap={8}>
             {itemData.map((item) => (
               <ImageListItem key={item.img}>
+                <motion.div
+                  variants={itemImg}
+                  initial={{opacity: 0}}
+                  animate={{opacity: 1}}
+                >
                  <Image
                   src={item.img}
-                  // component={motion.div}
-                  variants={itemImg}
                   initial="hidden"
                   animate="visible"
-                  // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  object-fit="cover"
                   alt={item.imgTitle}
                   loading="lazy"
                 />
+                </motion.div>
               </ImageListItem>
             ))}
           </ImageList>
-      </div>
+          </motion.div>
+      </motion.div>
     </DivModal>
     </AnimatePresence>
   );
